01352f7d4ed104f3a8c459d3de8f4b4ecb32ceeb
MorehouseJ09 <morehousej09@gmail.com>
2012-12-23 06:35:03 -0500

    Initialized require js for project

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+require(["jquery", "test/base"], function($) {
+    //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
+    $(function() {
+
+    	// LOAD THE INITIAL FUNCTIONALITY HERE
+
+
+    });
+});
