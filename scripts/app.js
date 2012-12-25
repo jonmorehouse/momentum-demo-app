28c44c0fb7361a72f131989904d8c55c7afbef30
MorehouseJ09 <morehousej09@gmail.com>
2012-12-25 04:42:05 -0500

    Initialized require.js / modernizr / jquery - untested

---
+++ scripts/app.js
@@ -0,0 +1,30 @@
+requirejs.config({
+
+	// make sure that our 
+	urlArgs: "bust=" + (new Date()).getTime(),//used to keep the browser from caching the scripts as we move
+	baseUrl : "scripts",//base scripts page!
+
+
+	paths : {	
+
+		'jquery' : "../components/jquery/jquery.min", //specific libraries -- can be specified later
+		'paper' : "../components/paper/paper"
+	},
+
+	shim: {
+
+        'paper' : {
+        	exports: 'paper'
+        },
+    },
+});
+
+// initialize the document with a doc ready!
+requirejs(["modernizr"], function() {
+
+	requirejs(["jquery"], function ($) {
+
+		
+
+	});
+});
